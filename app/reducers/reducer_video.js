ad65e7c1dd921b8b1fdf76ad960ea7877129eb1d
Lim Jun Xian <eldrethlim@gmail.com>
2016-08-31 03:00:48 +0800

    refactored logic from container to action

--- app/reducers/reducer_video.js
+++ app/reducers/reducer_video.js
@@ -1,5 +1,11 @@
 export default function(state = null, action) {
   switch(action.type) {
+  case 'VIDEOS_SEARCHED':
+    if (!state) {
+      return action.payload.data.items[0];
+    } else {
+      return state;
+    }
   case 'VIDEO_SELECTED':
     return action.payload;
   }
